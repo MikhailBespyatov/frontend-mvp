b6b30e1791bec6bc43947123e9f14c5668b900ca
mikhail <besp9tov.mikhail94@gmail.com>
2022-09-28 19:57:34 +0600

    fix scoring button

--- src/components/Steps/Scoring/Scoring.tsx
+++ src/components/Steps/Scoring/Scoring.tsx
@@ -1,122 +1,157 @@
-import { useEffect, useState } from 'react';
-import { useAtom } from 'jotai';
-import clsx from 'clsx';
-import { Typography } from '@alfalab/core-components/typography';
+import { useEffect, useState } from "react";
+import { useAtom } from "jotai";
+import clsx from "clsx";
+import { Typography } from "@alfalab/core-components/typography";
 
-import configs from './configs/configs';
+import configs from "./configs/configs";
 
-import HttpService from 'HttpService/HttpService';
+import HttpService from "HttpService/HttpService";
 
-import { _error, _errorCode, _partner, _paymentSchedule, _redirectLinkToMarket, _step } from 'store/store';
-import { ERROR_CASES } from '../../../constants';
+import {
+  _error,
+  _errorCode,
+  _partner,
+  _paymentSchedule,
+  _redirectLinkToMarket,
+  _step,
+} from "store/store";
+import { ERROR_CASES } from "../../../constants";
 
-import { clouds } from 'configs/mode_change';
-import styles from './Scoring.module.css';
-import cn from 'classnames';
-import { PERCENT } from './constants';
-import UIButton from '../../../ui-components/Button/Button';
+import { clouds } from "configs/mode_change";
+import styles from "./Scoring.module.css";
+import cn from "classnames";
+import { PERCENT } from "./constants";
+import UIButton from "../../../ui-components/Button/Button";
 
 export default function Scoring(): JSX.Element {
-    const [redirectLinkToMarket] = useAtom(_redirectLinkToMarket);
-    const [step, setStep] = useAtom(_step);
-    const [, setPartner] = useAtom(_partner);
-    const [, setPaymentShedule] = useAtom(_paymentSchedule);
-    const [error, setError] = useAtom(_error);
-    const [, setErrorCode] = useAtom(_errorCode);
+  const [redirectLinkToMarket] = useAtom(_redirectLinkToMarket);
+  const [step, setStep] = useAtom(_step);
+  const [, setPartner] = useAtom(_partner);
+  const [, setPaymentShedule] = useAtom(_paymentSchedule);
+  const [error, setError] = useAtom(_error);
+  const [, setErrorCode] = useAtom(_errorCode);
 
-    const [scoringStep, setScoringStep] = useState(0);
+  const [scoringStep, setScoringStep] = useState(0);
 
-    const { title, image: Image } = configs[scoringStep];
+  const { title, image: Image } = configs[scoringStep];
 
-    useEffect(() => {
-        getStatus();
-    }, []);
+  useEffect(() => {
+    getStatus();
+  }, []);
 
-    const timeoutId = setTimeout(() => setScoringStep(scoringStep + 1), 30000);
+  const timeoutId = setTimeout(() => setScoringStep(scoringStep + 1), 30000);
 
+  if (scoringStep === 3) {
+    clearTimeout(timeoutId);
+  }
+
+  useEffect(() => {
     if (scoringStep === 3) {
-        clearTimeout(timeoutId);
+      setStep(step + 1);
     }
+  }, [scoringStep]);
 
-    useEffect(() => {
-        if (scoringStep === 3) {
-            setStep(step + 1);
+  const getStatus = () => {
+    HttpService.getStatus()
+      .then((res) => {
+        const {
+          partnerData: { partnerName },
+          terms,
+          code,
+          redirectLink,
+        } = res.data.data;
+
+        if (res.status === 200) {
+          if (code === ERROR_CASES.ECOM_INTERNAL_ERROR) {
+            window.location.href = redirectLink;
+          }
+          setPartner(partnerName);
+          setPaymentShedule(terms);
+          setScoringStep(3);
+          setTimeout(() => setStep(step + 1), 1000);
         }
-    }, [scoringStep]);
 
-    const getStatus = () => {
-        HttpService.getStatus()
-            .then((res) => {
-                const {
-                    partnerData: { partnerName },
-                    terms,
-                    code,
-                    redirectLink,
-                } = res.data.data;
+        if (res.status === 202) {
+          setTimeout(() => {
+            getStatus();
+          }, 2000);
+        }
+      })
+      .catch((err) => {
+        const { code } = err.response.data.error;
 
-                if (res.status === 200) {
-                    if (code === ERROR_CASES.ECOM_INTERNAL_ERROR) {
-                        window.location.href = redirectLink;
-                    }
-                    setPartner(partnerName);
-                    setPaymentShedule(terms);
-                    setScoringStep(3);
-                    setTimeout(() => setStep(step + 1), 1000);
-                }
+        setErrorCode(code);
+        setError(!error);
+      });
+  };
 
-                if (res.status === 202) {
-                    setTimeout(() => {
-                        getStatus();
-                    }, 2000);
-                }
-            })
-            .catch((err) => {
-                const { code } = err.response.data.error;
+  const goBack = () => {
+    window.location.href = redirectLinkToMarket;
+  };
 
-                setErrorCode(code);
-                setError(!error);
-            });
-    };
+  return (
+    <div className={styles.main_container}>
+      <div>
+        <img
+          alt="Cloud one"
+          className={clsx(styles.cloud, styles.cloud_one)}
+          src={clouds.one}
+        />
+        <img
+          alt="Cloud two"
+          className={clsx(styles.cloud, styles.cloud_two)}
+          src={clouds.two}
+        />
+        <img
+          alt="Cloud three"
+          className={clsx(styles.cloud, styles.cloud_three)}
+          src={clouds.three}
+        />
 
-    const goBack = () => {
-        window.location.href = redirectLinkToMarket;
-    };
+        <Image className={styles.img} />
+        <div className={styles.line_wrapper}>
+          <span className={clsx(styles.line, styles.line_active)}></span>
+          <span
+            className={clsx(styles.line, {
+              [styles.line_active]: scoringStep > 0,
+            })}
+          ></span>
+          <span
+            className={clsx(styles.line, {
+              [styles.line_active]: scoringStep > 1,
+            })}
+          ></span>
+          <span
+            className={clsx(styles.line, {
+              [styles.line_active]: scoringStep > 2,
+            })}
+          ></span>
+        </div>
+        <div className={styles.title_with_subtitle}>
+          <div className={styles.single_title}>
+            <Typography.Title className={styles.title} tag="h1">
+              Это займёт 30 секунд
+            </Typography.Title>
+          </div>
 
-    return (
-        <div className={styles.main_container}>
-            <div>
-                <img alt="Cloud one" className={clsx(styles.cloud, styles.cloud_one)} src={clouds.one} />
-                <img alt="Cloud two" className={clsx(styles.cloud, styles.cloud_two)} src={clouds.two} />
-                <img alt="Cloud three" className={clsx(styles.cloud, styles.cloud_three)} src={clouds.three} />
+          <div className={styles.title_wrapper}>
+            <Typography.Title className={styles.title} tag="h1">
+              {title}
+            </Typography.Title>
+          </div>
+        </div>
+        <div className={styles.progress_wrapper}>
+          <div className={styles.initial_progress}>
+            <span className={styles.progress_percent}>
+              {PERCENT[scoringStep as keyof typeof PERCENT]}
+            </span>
+            <div
+              className={cn(styles.progress, styles[`progress_${scoringStep}`])}
+            ></div>
+          </div>
+        </div>
 
-                <Image className={styles.img} />
-                <div className={styles.line_wrapper}>
-                    <span className={clsx(styles.line, styles.line_active)}></span>
-                    <span className={clsx(styles.line, { [styles.line_active]: scoringStep > 0 })}></span>
-                    <span className={clsx(styles.line, { [styles.line_active]: scoringStep > 1 })}></span>
-                    <span className={clsx(styles.line, { [styles.line_active]: scoringStep > 2 })}></span>
-                </div>
-                <div className={styles.title_with_subtitle}>
-                    <div className={styles.single_title}>
-                        <Typography.Title className={styles.title} tag="h1">
-                            Это займёт 30 секунд
-                        </Typography.Title>
-                    </div>
-
-                    <div className={styles.title_wrapper}>
-                        <Typography.Title className={styles.title} tag="h1">
-                            {title}
-                        </Typography.Title>
-                    </div>
-                </div>
-                <div className={styles.progress_wrapper}>
-                    <div className={styles.initial_progress}>
-                        <span className={styles.progress_percent}>{PERCENT[scoringStep as keyof typeof PERCENT]}</span>
-                        <div className={cn(styles.progress, styles[`progress_${scoringStep}`])}></div>
-                    </div>
-                </div>
-
-                {/* <div className={styles.loader_wrapper}>
+        {/* <div className={styles.loader_wrapper}>
                     <div className={styles.loader}>
                         {Array(12)
                             .fill('')
@@ -125,8 +160,15 @@ export default function Scoring(): JSX.Element {
                             ))}
                     </div>
                 </div> */}
-            </div>
-            <UIButton btnType="continue" change={goBack} disable={false} title="Вернуться в магазин" />
-        </div>
-    );
+      </div>
+      <div className={styles.buttonWrapper}>
+        <UIButton
+          btnType="continue"
+          change={goBack}
+          disable={false}
+          title="Вернуться в магазин"
+        />
+      </div>
+    </div>
+  );
 }
--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,19 +1,22 @@
-import { initialData, initialErrors } from 'components/Steps/LinkingCard/constants';
-import { atom } from 'jotai';
+import {
+  initialData,
+  initialErrors,
+} from "components/Steps/LinkingCard/constants";
+import { atom } from "jotai";
 
-import { ApplicationStep } from './store.types';
+import { ApplicationStep } from "./store.types";
 
-export const _user = atom<IUser>({ iin: '', phoneNumber: '' });
-export const _preappId = atom<string>('');
-export const _expire = atom<string>('');
-export const _partner = atom<string>('');
-export const _partnerCode = atom<string>('');
-export const _step = atom<number>(ApplicationStep.VERIFY);
-export const _redirectLinkToMarket = atom<string>('');
+export const _user = atom<IUser>({ iin: "", phoneNumber: "" });
+export const _preappId = atom<string>("");
+export const _expire = atom<string>("");
+export const _partner = atom<string>("");
+export const _partnerCode = atom<string>("");
+export const _step = atom<number>(ApplicationStep.SCORING);
+export const _redirectLinkToMarket = atom<string>("");
 export const _paymentSchedule = atom<IPaymentSchedule[]>([]);
 export const _countdownDuration = atom<number>(60000);
-export const _cardRequestLink = atom<string>('');
-export const _firstDayPay = atom<string>('');
+export const _cardRequestLink = atom<string>("");
+export const _firstDayPay = atom<string>("");
 export const _userCards = atom<IUserCard[]>([]);
 
 export const _error = atom<boolean>(false);
@@ -24,4 +27,6 @@ export const _cardErrors = atom(initialErrors);
 
 export const _activeSavedCard = atom<IUserCard | null>(null);
 
-export const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+export const isDarkMode = window.matchMedia(
+  "(prefers-color-scheme: dark)"
+).matches;
